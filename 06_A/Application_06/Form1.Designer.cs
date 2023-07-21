a6ff13fe9b5745127ea418f0aebca3bf17bdad93
Nalin Dhingra <50366950+Lotus98@users.noreply.github.com>
2023-07-22 01:33:16 +0200

    Add Application 6

---
+++ 06_A/Application_06/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace Application_06 {
+    partial class Form1 {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            pictureBox = new PictureBox();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            nUD = new NumericUpDown();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            mUD = new NumericUpDown();
+            groupBox3 = new GroupBox();
+            label3 = new Label();
+            epsUD = new NumericUpDown();
+            groupBox4 = new GroupBox();
+            label4 = new Label();
+            pUD = new NumericUpDown();
+            groupBox5 = new GroupBox();
+            label5 = new Label();
+            jUD = new NumericUpDown();
+            groupBox6 = new GroupBox();
+            whichFreq = new ComboBox();
+            computeButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nUD).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)mUD).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)epsUD).BeginInit();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pUD).BeginInit();
+            groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)jUD).BeginInit();
+            groupBox6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox.Location = new Point(14, 111);
+            pictureBox.Margin = new Padding(4, 3, 4, 3);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(1388, 519);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(nUD);
+            groupBox1.Location = new Point(14, 14);
+            groupBox1.Margin = new Padding(4, 3, 4, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.Size = new Size(175, 70);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Insert number of rounds (n)";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(7, 24);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 1;
+            label1.Text = "n:";
+            // 
+            // nUD
+            // 
+            nUD.Location = new Point(33, 22);
+            nUD.Margin = new Padding(4, 3, 4, 3);
+            nUD.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            nUD.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nUD.Name = "nUD";
+            nUD.Size = new Size(107, 23);
+            nUD.TabIndex = 0;
+            nUD.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(mUD);
+            groupBox2.Location = new Point(226, 14);
+            groupBox2.Margin = new Padding(4, 3, 4, 3);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(4, 3, 4, 3);
+            groupBox2.Size = new Size(174, 70);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Insert number of paths (m)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(7, 24);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(21, 15);
+            label2.TabIndex = 1;
+            label2.Text = "m:";
+            // 
+            // mUD
+            // 
+            mUD.Location = new Point(33, 22);
+            mUD.Margin = new Padding(4, 3, 4, 3);
+            mUD.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            mUD.Name = "mUD";
+            mUD.Size = new Size(107, 23);
+            mUD.TabIndex = 0;
+            mUD.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(epsUD);
+            groupBox3.Location = new Point(431, 14);
+            groupBox3.Margin = new Padding(4, 3, 4, 3);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Padding = new Padding(4, 3, 4, 3);
+            groupBox3.Size = new Size(174, 70);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Insert epsilon";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(7, 24);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 15);
+            label3.TabIndex = 1;
+            label3.Text = "epsilon:";
+            // 
+            // epsUD
+            // 
+            epsUD.DecimalPlaces = 2;
+            epsUD.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
+            epsUD.Location = new Point(59, 22);
+            epsUD.Margin = new Padding(4, 3, 4, 3);
+            epsUD.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
+            epsUD.Minimum = new decimal(new int[] { 1, 0, 0, 131072 });
+            epsUD.Name = "epsUD";
+            epsUD.Size = new Size(107, 23);
+            epsUD.TabIndex = 0;
+            epsUD.Value = new decimal(new int[] { 1, 0, 0, 131072 });
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label4);
+            groupBox4.Controls.Add(pUD);
+            groupBox4.Location = new Point(647, 14);
+            groupBox4.Margin = new Padding(4, 3, 4, 3);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Padding = new Padding(4, 3, 4, 3);
+            groupBox4.Size = new Size(192, 70);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Insert probability of hit (0<p<1)";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(7, 24);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(17, 15);
+            label4.TabIndex = 1;
+            label4.Text = "p:";
+            // 
+            // pUD
+            // 
+            pUD.DecimalPlaces = 2;
+            pUD.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
+            pUD.Location = new Point(33, 22);
+            pUD.Margin = new Padding(4, 3, 4, 3);
+            pUD.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
+            pUD.Name = "pUD";
+            pUD.Size = new Size(107, 23);
+            pUD.TabIndex = 0;
+            pUD.Value = new decimal(new int[] { 5, 0, 0, 65536 });
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(label5);
+            groupBox5.Controls.Add(jUD);
+            groupBox5.Location = new Point(14, 434);
+            groupBox5.Margin = new Padding(4, 3, 4, 3);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Padding = new Padding(4, 3, 4, 3);
+            groupBox5.Size = new Size(214, 70);
+            groupBox5.TabIndex = 3;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Insert intermediate step (0 < j < n)";
+            groupBox5.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(7, 27);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(13, 15);
+            label5.TabIndex = 1;
+            label5.Text = "j:";
+            // 
+            // jUD
+            // 
+            jUD.Location = new Point(28, 22);
+            jUD.Margin = new Padding(4, 3, 4, 3);
+            jUD.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            jUD.Name = "jUD";
+            jUD.Size = new Size(107, 23);
+            jUD.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(whichFreq);
+            groupBox6.Location = new Point(876, 14);
+            groupBox6.Margin = new Padding(4, 3, 4, 3);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Padding = new Padding(4, 3, 4, 3);
+            groupBox6.Size = new Size(214, 70);
+            groupBox6.TabIndex = 4;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Choose frequency";
+            // 
+            // whichFreq
+            // 
+            whichFreq.FormattingEnabled = true;
+            whichFreq.Items.AddRange(new object[] { "Relative", "Absolute", "Normalized" });
+            whichFreq.Location = new Point(18, 27);
+            whichFreq.Name = "whichFreq";
+            whichFreq.Size = new Size(121, 23);
+            whichFreq.TabIndex = 0;
+            whichFreq.Text = "-- Select --";
+            whichFreq.SelectedIndexChanged += whichFreq_SelectedIndexChanged;
+            // 
+            // computeButton
+            // 
+            computeButton.Location = new Point(1158, 23);
+            computeButton.Margin = new Padding(4, 3, 4, 3);
+            computeButton.Name = "computeButton";
+            computeButton.Size = new Size(160, 44);
+            computeButton.TabIndex = 5;
+            computeButton.Text = "Compute";
+            computeButton.UseVisualStyleBackColor = true;
+            computeButton.Click += computeButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1416, 644);
+            Controls.Add(groupBox6);
+            Controls.Add(computeButton);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Bernoulli Distribution convergence";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nUD).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)mUD).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)epsUD).EndInit();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pUD).EndInit();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)jUD).EndInit();
+            groupBox6.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private GroupBox groupBox1;
+        private Label label1;
+        private NumericUpDown nUD;
+        private GroupBox groupBox2;
+        private Label label2;
+        private NumericUpDown mUD;
+        private GroupBox groupBox3;
+        private Label label3;
+        private NumericUpDown epsUD;
+        private GroupBox groupBox4;
+        private Label label4;
+        private NumericUpDown pUD;
+        private GroupBox groupBox5;
+        private Label label5;
+        private NumericUpDown jUD;
+        private Button computeButton;
+        private GroupBox groupBox6;
+        private ComboBox whichFreq;
+    }
+}
+
---
+++ 06_A/Application_06/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StatisticsLib;
+
+namespace Application_06 {
+    public partial class Form1 : Form {
+        // Data
+        private ContinuousDistribution relFreq, absFreq, normFreq; // Used to draw histograms
+        private PointF[][] absP, relP, normP;
+        private int N, M, J;
+        private float XvMax;
+        private double EPS, P;
+        private Random rand = new Random();
+        private Pen[] pens;
+
+        public Form1() {
+            InitializeComponent();
+
+            // Initialize Viewport width destined to paths drawing
+            XvMax = (float)(pictureBox.Width - (pictureBox.Width / 10));      // Take out 1/10 for the final histogram distribution
+            
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+        }
+
+        // Methods
+        private Pen RandomPen() {
+            return new Pen(Color.FromArgb((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255)));
+        }
+
+        private PointF WtoV(float Xw, float Yw,
+                            float XwMin, float YwMin,
+                            float XwMax, float YwMax,
+                            float XvMin, float YvMin,
+                            float XvMax, float YvMax) {
+            float Sx, Sy, Xv, Yv;
+
+            Sx = (XvMax - XvMin) / (XwMax - XwMin);
+            Sy = (YvMax - YvMin) / (YwMax - YwMin);
+
+            Xv = XvMin + ((Xw - XwMin) * Sx);
+            Yv = YvMax - ((Yw - YwMin) * Sy); // Because the coordinates in the Viewport start from top-left
+
+            return new PointF(Xv, Yv);
+        }
+
+        private void computeButton_Click(object sender, EventArgs e) {
+            // Collect values
+            N = (int)nUD.Value;
+            M = (int)mUD.Value;
+            J = (int)jUD.Value;
+            P = (double)pUD.Value;
+            EPS = (double)epsUD.Value;
+
+            // Sanity checks
+            if (J >= N) {
+                MessageBox.Show("J should be a value between 0 and N", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Initialize distributions to draw histograms
+            relFreq = new ContinuousDistribution("Relative", 0.0, 1.0, EPS);
+            absFreq = new ContinuousDistribution("Absolute", 0.0, N, N*EPS);
+            normFreq = new ContinuousDistribution("Normalized", 0.0, N / Math.Sqrt(N), (N / Math.Sqrt(N))*EPS);
+
+            // Initialize Points matrices (jaggered arrays) to draw distribution paths
+            absP = new PointF[M][];
+            relP = new PointF[M][];
+            normP = new PointF[M][];
+
+            
+            // Declare extra vars
+            int[] success = new int[M];          // Needed to calculate absolute frequency
+            OnlineMean[] means = new OnlineMean[M];     // Needed to calculate relative frequencies (also normalized)
+            pens = new Pen[M];
+
+            //// Initialize distribution for intermediate histogram
+            //ContinuousDistribution jDistrib = new ContinuousDistribution("jdistrib", 0.0, 1.0, EPS);
+
+            // Initialize variables according to number of paths
+            for (int i = 0; i < M; i++) {
+                success[i] = 0;
+                means[i] = new OnlineMean();
+                pens[i] = RandomPen();
+                // Initializing arrays of points
+                absP[i] = new PointF[N];
+                relP[i] = new PointF[N];
+                normP[i] = new PointF[N];
+            }
+
+            // Start generating Bernoulli distributions
+            for (int i = 0; i < N; i++) {
+
+                for (int j = 0; j < M; j++) {
+                    double val = rand.NextDouble();
+                    if (val <= P) {
+                        success[j]++;
+                        means[j].AddValue(1);
+                    }
+                    else {
+                        means[j].AddValue(0);
+                    }
+
+                    //if (i < J) {
+                    //    jDistrib.InsertInList(freqs[j].Avg); // Intermediate histogram
+                    //}
+
+                    relP[j][i] = WtoV(i, (float)means[j].Avg, 0, 0, N, 1, 0, 0, XvMax, pictureBox.Height);
+                    absP[j][i] = WtoV(i, success[j], 0, 0, N, N, 0, 0, XvMax, pictureBox.Height);
+                    normP[j][i] = WtoV(i, (float)(success[j]/Math.Sqrt(i+1)), 0, 0, N, (float)(N/Math.Sqrt(N)), 0, 0, XvMax, pictureBox.Height);
+                }
+            }
+
+            // Filling distributions
+            for (int i = 0; i < M; i++) {
+                relFreq.InsertInList(means[i].Avg);
+                absFreq.InsertInList(success[i]);
+                normFreq.InsertInList(success[i] / Math.Sqrt(N));
+            }
+
+            if (whichFreq.Text == "Relative")
+                DrawRelative(N, M);
+            else if (whichFreq.Text == "Absolute")
+                DrawAbsolute(N, M);
+            else
+                DrawNormalized(N, M);
+
+            
+
+            // Draw intermediate histograms
+            //distrib = jDistrib.Distribution;
+            //maxCount = 0;
+            //for (int j = (distrib.Count - 1); j >= 0; j--) {
+            //    if (maxCount < distrib[j].Count) {
+            //        maxCount = distrib[j].Count;
+            //    }
+            //}
+            //colRatio = (double)(pictureBox.Width / 10) / (double)maxCount;
+
+            //// Draw line
+            //p1 = new PointF((float)step * J, 0);
+            //p2 = new PointF((float)step * J, (float)pictureBox.Height);
+            //gfx.DrawLine(Pens.Red, p1, p2);
+
+            //// Weird loop to modify EPS to get finer granularity
+            //colRect = new RectangleF(p1, new SizeF(0, (float)histoStep));
+            //for (int j = (numCols - 1); j >= 0; j--) {
+            //    colRect.Width = ((float)(distrib[j].Count * colRatio));
+            //    gfx.FillRectangle(Brushes.Yellow, colRect);
+            //    gfx.DrawRectangle(Pens.Red, colRect.X, colRect.Y, colRect.Width, colRect.Height);
+            //    colRect.Y += (float)histoStep;
+            //}
+
+
+            return;
+        }
+
+        private void DrawRelative(int N, int M) {
+            List<Interval> distrib;
+
+            // Init drawing tools
+            Graphics gfx = Graphics.FromImage(pictureBox.Image);
+            gfx.Clear(pictureBox.BackColor);
+
+            // Draw distribution paths
+            for (int i = 0; i < M; i++) {
+                gfx.DrawLines(pens[i], relP[i]);
+            }
+
+            // Draw line that separates paths from histogram
+            PointF p1, p2;
+            p1 = new PointF(XvMax, 0);
+            p2 = new PointF(XvMax, pictureBox.Height);
+            gfx.DrawLine(Pens.Red, p1, p2);
+
+            // Draw histogram
+            distrib = relFreq.Distribution;
+            float rectH = (float)pictureBox.Height / distrib.Count;
+            for (int i = 0; i < distrib.Count; i++) {
+                // Getting the upper-right corner of the rectangle
+                PointF rectP = WtoV(distrib[i].Count, i + 1, 0, 0, M, distrib.Count, XvMax, 0, pictureBox.Width, pictureBox.Height);
+                //Debug.WriteLine("upper-right corner: [" + rectP.X + ", "+ rectP.Y + "]");
+                RectangleF colRect = new RectangleF(XvMax, rectP.Y, rectP.X - XvMax, rectH);
+                gfx.FillRectangle(Brushes.Yellow, colRect);
+                gfx.DrawRectangle(Pens.Red, colRect.X, colRect.Y, colRect.Width, colRect.Height);
+            }
+            //Debug.WriteLine("PictureBox width: " + pictureBox.Width);
+            //Debug.WriteLine("Number of intervals: " + distrib.Count);
+            //Debug.WriteLine("Viewport Histogram X: " + XvMax);
+            //Debug.WriteLine("Rectangle height: " + rectH);
+
+            gfx.Dispose();
+            pictureBox.Refresh();
+            return;
+        }
+
+        private void DrawAbsolute(int N, int M) {
+            List<Interval> distrib;
+
+            // Init drawing tools
+            Graphics gfx = Graphics.FromImage(pictureBox.Image);
+            gfx.Clear(pictureBox.BackColor);
+
+            // Draw distribution paths
+            for (int i = 0; i < M; i++) {
+                gfx.DrawLines(pens[i], absP[i]);
+            }
+
+            // Draw line that separates paths from histogram
+            PointF p1, p2;
+            p1 = new PointF(XvMax, 0);
+            p2 = new PointF(XvMax, pictureBox.Height);
+            gfx.DrawLine(Pens.Red, p1, p2);
+
+            // Draw histogram
+            distrib = absFreq.Distribution;
+            float rectH = (float)pictureBox.Height / distrib.Count;
+            for (int i = 0; i < distrib.Count; i++) {
+                // Getting the upper-right corner of the rectangle
+                PointF rectP = WtoV(distrib[i].Count, i + 1, 0, 0, M, distrib.Count, XvMax, 0, pictureBox.Width, pictureBox.Height);
+                //Debug.WriteLine("upper-right corner: [" + rectP.X + ", "+ rectP.Y + "]");
+                RectangleF colRect = new RectangleF(XvMax, rectP.Y, rectP.X - XvMax, rectH);
+                gfx.FillRectangle(Brushes.Yellow, colRect);
+                gfx.DrawRectangle(Pens.Red, colRect.X, colRect.Y, colRect.Width, colRect.Height);
+            }
+            //Debug.WriteLine("PictureBox width: " + pictureBox.Width);
+            //Debug.WriteLine("Number of intervals: " + distrib.Count);
+            //Debug.WriteLine("Viewport Histogram X: " + XvMax);
+            //Debug.WriteLine("Rectangle height: " + rectH);
+
+            gfx.Dispose();
+            pictureBox.Refresh();
+            return;
+        }
+
+        private void DrawNormalized(int N, int M) {
+            List<Interval> distrib;
+
+            // Init drawing tools
+            Graphics gfx = Graphics.FromImage(pictureBox.Image);
+            gfx.Clear(pictureBox.BackColor);
+
+            // Draw distribution paths
+            for (int i = 0; i < M; i++) {
+                gfx.DrawLines(pens[i], normP[i]);
+            }
+
+            // Draw line that separates paths from histogram
+            PointF p1, p2;
+            p1 = new PointF(XvMax, 0);
+            p2 = new PointF(XvMax, pictureBox.Height);
+            gfx.DrawLine(Pens.Red, p1, p2);
+
+            // Draw histogram
+            distrib = normFreq.Distribution;
+            float rectH = (float)pictureBox.Height / distrib.Count;
+            for (int i = 0; i < distrib.Count; i++) {
+                // Getting the upper-right corner of the rectangle
+                PointF rectP = WtoV(distrib[i].Count, i + 1, 0, 0, M, distrib.Count, XvMax, 0, pictureBox.Width, pictureBox.Height);
+                //Debug.WriteLine("upper-right corner: [" + rectP.X + ", "+ rectP.Y + "]");
+                RectangleF colRect = new RectangleF(XvMax, rectP.Y, rectP.X - XvMax, rectH);
+                gfx.FillRectangle(Brushes.Yellow, colRect);
+                gfx.DrawRectangle(Pens.Red, colRect.X, colRect.Y, colRect.Width, colRect.Height);
+            }
+            //Debug.WriteLine("PictureBox width: " + pictureBox.Width);
+            //Debug.WriteLine("Number of intervals: " + distrib.Count);
+            //Debug.WriteLine("Viewport Histogram X: " + XvMax);
+            //Debug.WriteLine("Rectangle height: " + rectH);
+
+            gfx.Dispose();
+            pictureBox.Refresh();
+            return;
+        }
+
+        private void whichFreq_SelectedIndexChanged(object sender, EventArgs e) {
+            if (relFreq == null || absFreq == null || normFreq == null) 
+                return;
+
+            if (whichFreq.Text == "Relative")
+                DrawRelative(N, M);
+            else if (whichFreq.Text == "Absolute")
+                DrawAbsolute(N, M);
+            else if (whichFreq.Text == "Normalized")
+                DrawNormalized(N, M);
+        }
+    }
+}
