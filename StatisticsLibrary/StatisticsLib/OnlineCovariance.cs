c9d711a4ead2a743f8832340bf2b2d9a77f96786
Nalin Dhingra <50366950+Lotus98@users.noreply.github.com>
2023-10-27 18:29:08 +0200

    [Library] Update: Onlince variances definitions

---
+++ StatisticsLibrary/StatisticsLib/OnlineCovariance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsLib {
+    public class OnlineCovariance {
+        // Data needed
+        public OnlineMean meanX, meanY;
+        private double sumCross;
+        private int n;
+
+        public OnlineCovariance() {
+            meanX = new OnlineMean();
+            meanY = new OnlineMean();
+            sumCross = 0;
+            n = 0;
+        }
+
+        // Properties
+        public double Covariance { get => (sumCross/n); }
+
+        // Methods
+        public void AddValues(double x, double y) {
+            // Calculate x term before updating the mean of x
+            double xTerm = x - meanX.Avg;
+
+            // Update means
+            meanX.AddValue(x);
+            meanY.AddValue(y);
+
+            // calculate y term
+            double yTerm = y - meanY.Avg;
+
+            // Update sum of cross products
+            sumCross += xTerm * yTerm;
+            n++;
+        }
+
+
+    }
+}
---
+++ StatisticsLibrary/StatisticsLib/OnlineVariance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsLib {
+
+    // We can reuse the Covariance class to compute the variance
+    public class OnlineVariance : OnlineCovariance{
+
+        public OnlineVariance() : base() { }
+
+        // Properties
+        public double Variance { get => this.Covariance; }
+
+        // Methods
+        public void AddValue(double x) {
+            this.AddValues(x, x);
+        }
+    }
+}
