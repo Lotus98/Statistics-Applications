b7cd6368e540ad55c23a6ecbce1d95b1dd23cf33
Nalin Dhingra <50366950+Lotus98@users.noreply.github.com>
2024-01-04 17:53:43 +0100

    Add Application 12

---
+++ 12_A/Application12/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Application12 {
+    partial class Form1 {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.simulationPictureBox = new System.Windows.Forms.PictureBox();
+            this.vertHisto = new System.Windows.Forms.PictureBox();
+            this.horizHisto = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simulationPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vertHisto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horizHisto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(111, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of trials:";
+            // 
+            // simulationPictureBox
+            // 
+            this.simulationPictureBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.simulationPictureBox.Location = new System.Drawing.Point(12, 69);
+            this.simulationPictureBox.Name = "simulationPictureBox";
+            this.simulationPictureBox.Size = new System.Drawing.Size(540, 540);
+            this.simulationPictureBox.TabIndex = 1;
+            this.simulationPictureBox.TabStop = false;
+            // 
+            // vertHisto
+            // 
+            this.vertHisto.Location = new System.Drawing.Point(572, 69);
+            this.vertHisto.Name = "vertHisto";
+            this.vertHisto.Size = new System.Drawing.Size(593, 265);
+            this.vertHisto.TabIndex = 2;
+            this.vertHisto.TabStop = false;
+            // 
+            // horizHisto
+            // 
+            this.horizHisto.Location = new System.Drawing.Point(572, 344);
+            this.horizHisto.Name = "horizHisto";
+            this.horizHisto.Size = new System.Drawing.Size(593, 265);
+            this.horizHisto.TabIndex = 3;
+            this.horizHisto.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Simulation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Application12Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 620);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.horizHisto);
+            this.Controls.Add(this.vertHisto);
+            this.Controls.Add(this.simulationPictureBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Application12Form1";
+            this.Text = "Application 12";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simulationPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vertHisto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horizHisto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox simulationPictureBox;
+        private System.Windows.Forms.PictureBox vertHisto;
+        private System.Windows.Forms.PictureBox horizHisto;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 12_A/Application12/Form1.cs
@@ -0,0 +1,101 @@
+﻿using StatisticsLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application12 {
+    public partial class Form1 : Form {
+        private float MAX_RADIUS;
+        private ContinuousDistribution xDist, yDist;
+        private Histogram xHist, yHist;
+        private DynamicRectangle xRect, yRect;
+        private PointF center;
+        private List<PointF> points;
+        private Random rand;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Set the maximum radius to the minimum of the width and height of the picture box 
+            MAX_RADIUS = Math.Min(simulationPictureBox.Width, simulationPictureBox.Height) / 2.0f;
+            // Set coordinates of the center of the picture box (these are used once we find the cartesian coordinates)
+            center = new PointF(simulationPictureBox.Width / 2.0f, simulationPictureBox.Height / 2.0f);
+            rand = new Random();
+            // Initialize picturebox
+            simulationPictureBox.Image = new Bitmap(simulationPictureBox.Width, simulationPictureBox.Height);
+            // Initialize histogram pictureboxes and their dynamic rectangles
+            vertHisto.Image = new Bitmap(vertHisto.Width, vertHisto.Height);
+            horizHisto.Image = new Bitmap(horizHisto.Width, horizHisto.Height);
+            xRect = new DynamicRectangle(new Rectangle(new Point(0, 0), horizHisto.Size), horizHisto, Color.Black);
+            yRect = new DynamicRectangle(new Rectangle(new Point(0, 0), vertHisto.Size), vertHisto, Color.Black);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int n = (int)numericUpDown1.Value;
+            // Initialize points list
+            points = new List<PointF>();
+
+            // Initialize the distributions
+            xDist = new ContinuousDistribution("xDist", -MAX_RADIUS, MAX_RADIUS, 20);
+            yDist = new ContinuousDistribution("yDist", -MAX_RADIUS, MAX_RADIUS, 20);
+
+            for (int i = 0; i < n; i++)
+            {
+                // Get random values for radius and angle
+                double radius = rand.NextDouble() * MAX_RADIUS;
+                double angle = rand.NextDouble() * 2 * Math.PI;
+
+                // Convert the polar coordinates to cartesian coordinates with respect to the center of the picture box
+                float x = (float)(radius * Math.Cos(angle) + center.X);
+                float y = (float)(radius * Math.Sin(angle) + center.Y);
+                xDist.InsertInList(x);
+                yDist.InsertInList(y);
+
+                points.Add(new PointF(x, y));
+            }
+
+            // Initialize the histograms
+            xHist = new Histogram(xDist, false);
+            yHist = new Histogram(yDist, true);
+            // Assign the histograms to the picture boxes
+            xRect.drawing = xHist;
+            yRect.drawing = yHist;
+
+            // Clear the picture box and draw the points
+            Graphics gfx = Graphics.FromImage(simulationPictureBox.Image);
+            gfx.Clear(simulationPictureBox.BackColor);
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            foreach (PointF p in points)
+            {
+                gfx.FillEllipse(Brushes.Aqua, p.X, p.Y, 2f, 2f);
+            }
+            simulationPictureBox.Refresh();
+            gfx.Dispose();
+
+            // Draw the histograms
+            gfx = Graphics.FromImage(horizHisto.Image);
+            gfx.Clear(Color.White);
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            xRect.Draw(gfx);
+            horizHisto.Refresh();
+            gfx.Dispose();
+
+            gfx = Graphics.FromImage(vertHisto.Image);
+            gfx.Clear(Color.White);
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            yRect.Draw(gfx);
+            vertHisto.Refresh();
+            gfx.Dispose();
+        }
+
+    }
+}
